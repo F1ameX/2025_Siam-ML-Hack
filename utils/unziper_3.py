d40c55374671cfa39b840bb17e941802f2e182b3
melnikovknst <melnikovknst@gmail.com>
2025-03-06 20:56:03 +0700

    updated

--- utils/unziper_3.py
+++ utils/unziper_3.py
@@ -7,12 +7,11 @@ def extract(zip_path, extract_path):
         zipf.extractall(extract_path)
 
     os.chdir(extract_path)
-    for item in ['.DS_Store', '__MACOSX']:
-        path = os.path.join(extract_path, item)
-        if os.path.isfile(path):
-            os.remove(path)
-        elif os.path.isdir(path):
-            shutil.rmtree(path)
+    if '.DS_Store' in os.listdir():
+        os.remove('.DS_Store')
+
+    if '__MACOSX' in os.listdir():
+        shutil.rmtree('__MACOSX')
 
 extract('src/raw_data/train_denoised.zip', 'src/train_denoised/')
 extract('src/raw_data/test_denoised.zip', 'src/test_denoised/')
